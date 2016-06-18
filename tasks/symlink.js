6fbee0cf8d164300d25de153093d6138c2f211cb
tim neutkens <tim@weprovide.com>
2016-06-18 15:30:20 +0200

    Indentation of variables

--- tasks/symlink.js
+++ tasks/symlink.js
@@ -1,11 +1,11 @@
 module.exports = function() {
   // Global variables
   var plugins = this.opts.plugins,
-    config  = this.opts.configs;
+      config  = this.opts.configs;
 
   // Create a relative symlink from <project root>/tools to the root of this package
-  const fs = require('fs');
-  const path = require('path');
+  const fs = require('fs'),
+        path = require('path');
 
   const relativeDirectory = path.relative(config.projectPath, fs.realpathSync('./'));
   fs.symlink(relativeDirectory, config.projectPath + '/tools', 'dir', () => {
