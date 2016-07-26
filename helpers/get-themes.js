688f58f88d9e907ed61e7d90e76ef5be4b761437
Bartek Igielski <igloczek@gmail.com>
2016-07-26 17:51:23 +0200

    Inheritance of non-style theme files

--- helpers/get-themes.js
+++ helpers/get-themes.js
@@ -2,23 +2,23 @@
 module.exports = function (plugins, config) {
   return function () {
     const themeName = plugins.util.env.theme || false,
-          themes    = config.themes;
+          themes    = Object.keys(config.themes);
 
     // If themes is empty we throw a configuration error
-    if(themes.length === 0) {
+    if (themes.length === 0) {
       throw new plugins.util.PluginError({
         'plugin' : 'config',
         'message': plugins.errorMessage('You have to create <magento root>/dev/tools/frontools/configs/themes.json')
       })
     }
 
-    if (themeName && !themes[themeName]) {
+    if (themeName && themes.indexOf(themeName) === -1) {
       throw new plugins.util.PluginError({
         plugin : 'config',
         message: plugins.errorMessage('Theme provided using --theme <theme-name> is not available in <magento root>/dev/tools/frontools/configs/themes.json')
       });
     }
 
-    return themeName ? themes[themeName] : themes;
+    return themeName ? [themeName] : themes;
   }
 };
--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,42 +1,52 @@
 'use strict';
 module.exports = function () {
   // Global variables
-  const plugins = this.opts.plugins,
-        config  = this.opts.configs,
-        themes  = plugins.getThemes();
+  const plugins  = this.opts.plugins,
+        execSync = require('child_process').execSync,
+        config   = this.opts.configs,
+        themes   = plugins.getThemes(),
+        prod     = plugins.util.env.prod || false;
 
-  if (plugins.util.env.theme && !themes.default) {
-    plugins.util.log(
-      plugins.util.colors.red.bold('[Warining] ')
-      + plugins.util.colors.yellow('This tasks is designed only for LESS themes which use ')
-      + plugins.util.colors.blue('@magento-import')
-    );
-  }
-  else {
-    const execSync = require('child_process').execSync;
 
-    // Execute bin/magento dev:source:theme:deploy for every theme
-    themes.forEach(name => {
-      // Loop through locales, because you are required to specify a locale
-      config.themes[name].locale.forEach(locale => {
-        const theme = config.themes[name];
+  themes.forEach(name => {
+    const theme = config.themes[name];
+    if (theme.lang === 'scss') {
+      const src       = config.projectPath + theme.src,
+            dest      = config.projectPath + theme.dest,
+            srcPaths  = plugins.globby.sync(src + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' }),
+            destPaths = [];
 
-        // Only execute the command when this theme is a theme using @magento-import
-        if (theme.default) {
-          theme.locale.forEach(locale => {
-            // if it's default theme, create symlinks to styles files via Magento CLI
-            // porting "@magento-import" to node.js might be time consuming
-            // and it's not so useful for front-end developers
-            // execSync to keep process synchronous and wait till CLI do the job
-            execSync(config.projectPath + 'bin/magento dev:source-theme:deploy'
-              + ' --type=' + theme.lang
-              + ' --locale=' + locale
-              + ' --area=' + theme.area
-              + ' --theme=' + theme.vendor + '/' + theme.name
-              + ' ' + theme.files.join(' '));
-          });
-        }
+      if (theme.parent) {
+        const parentTheme     = config.themes[theme.parent],
+              parentSrc       = config.projectPath + parentTheme.src,
+              parentSrcPaths  = plugins.globby.sync(parentSrc + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' }),
+              parentDestPaths = [];
+
+        parentSrcPaths.forEach(srcPath => {
+          plugins.fs.symlinkSync(srcPath, srcPath.replace('/web', '').replace(parentSrc, dest));
+        });
+      }
+
+      srcPaths.forEach(srcPath => {
+        const destPath = srcPath.replace('/web', '').replace(src, dest);
+        plugins.fs.unlinkSync(destPath);
+        plugins.fs.symlinkSync(srcPath, destPath);
       });
-    });
-  }
+    }
+    else if (theme.lang === 'less' && theme.default) {
+      // Loop through locales, because you are required to specify a locale
+      theme.locale.forEach(locale => {
+        // if it's default theme, create symlinks to styles files via Magento CLI
+        // porting "@magento-import" to node.js might be time consuming
+        // and it's not so useful for front-end developers
+        // execSync to keep process synchronous and wait till CLI do the job
+        execSync(config.projectPath + 'bin/magento dev:source-theme:deploy'
+        + ' --type=' + theme.lang
+        + ' --locale=' + locale
+        + ' --area=' + theme.area
+        + ' --theme=' + theme.vendor + '/' + theme.name
+        + ' ' + theme.files.join(' '));
+      });
+    }
+  });
 };
