04911937d8a91f6e1957cd14915f2e584388a8cb
Bartek Igielski <igloczek@gmail.com>
2016-07-26 21:38:44 +0200

    Create theme static files per locale

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -10,20 +10,50 @@ module.exports = function () {
 
   themes.forEach(name => {
     const theme = config.themes[name];
-    if (!theme.default) {
-      const src       = config.projectPath + theme.src,
-            dest      = config.projectPath + theme.dest,
-            srcPaths  = plugins.globby.sync(src + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' }),
-            destPaths = [];
+    if (theme.default) {
+      // Loop through locales, because you are required to specify a locale
+      theme.locale.forEach(locale => {
+        // if it's default theme, create symlinks to styles files via Magento CLI
+        // porting "@magento-import" to node.js might be time consuming
+        // and it's not so useful for front-end developers
+        // execSync to keep process synchronous and wait till CLI do the job
+        execSync(
+          config.projectPath + 'bin/magento dev:source-theme:deploy'
+          + ' --type=' + theme.lang
+          + ' --locale=' + locale
+          + ' --area=' + theme.area
+          + ' --theme=' + theme.vendor + '/' + theme.name
+          + ' ' + theme.files.join(' ')
+        );
+      });
+    }
+    else {
+      theme.locale.forEach(locale => {
+        const src       = config.projectPath + theme.src,
+              dest      = config.projectPath + theme.dest + '/' + locale,
+              srcPaths  = plugins.globby.sync(src + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' }),
+              destPaths = [];
 
-      if (theme.parent) {
-        const parentTheme     = config.themes[theme.parent],
-              parentSrc       = config.projectPath + parentTheme.src,
-              parentSrcPaths  = plugins.globby.sync(parentSrc + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' }),
-              parentDestPaths = [];
+        if (theme.parent) {
+          const parentTheme     = config.themes[theme.parent],
+                parentSrc       = config.projectPath + parentTheme.src,
+                parentSrcPaths  = plugins.globby.sync(parentSrc + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' }),
+                parentDestPaths = [];
 
-        parentSrcPaths.forEach(srcPath => {
-          const destPath = srcPath.replace('/web', '').replace(parentSrc, dest);
+          parentSrcPaths.forEach(srcPath => {
+            const destPath = srcPath.replace('/web', '').replace(parentSrc, dest);
+            try {
+              plugins.fs.ensureFileSync(destPath);
+              plugins.fs.unlinkSync(destPath);
+            }
+            finally {
+              prod ? plugins.fs.copySync(srcPath, destPath) : plugins.fs.symlinkSync(srcPath, destPath);
+            }
+          });
+        }
+
+        srcPaths.forEach(srcPath => {
+          const destPath = srcPath.replace('/web', '').replace(src, dest);
           try {
             plugins.fs.ensureFileSync(destPath);
             plugins.fs.unlinkSync(destPath);
@@ -32,32 +62,6 @@ module.exports = function () {
             prod ? plugins.fs.copySync(srcPath, destPath) : plugins.fs.symlinkSync(srcPath, destPath);
           }
         });
-      }
-
-      srcPaths.forEach(srcPath => {
-        const destPath = srcPath.replace('/web', '').replace(src, dest);
-        try {
-          plugins.fs.ensureFileSync(destPath);
-          plugins.fs.unlinkSync(destPath);
-        }
-        finally {
-          prod ? plugins.fs.copySync(srcPath, destPath) : plugins.fs.symlinkSync(srcPath, destPath);
-        }
-      });
-    }
-    else {
-      // Loop through locales, because you are required to specify a locale
-      theme.locale.forEach(locale => {
-        // if it's default theme, create symlinks to styles files via Magento CLI
-        // porting "@magento-import" to node.js might be time consuming
-        // and it's not so useful for front-end developers
-        // execSync to keep process synchronous and wait till CLI do the job
-        execSync(config.projectPath + 'bin/magento dev:source-theme:deploy'
-        + ' --type=' + theme.lang
-        + ' --locale=' + locale
-        + ' --area=' + theme.area
-        + ' --theme=' + theme.vendor + '/' + theme.name
-        + ' ' + theme.files.join(' '));
       });
     }
   });
