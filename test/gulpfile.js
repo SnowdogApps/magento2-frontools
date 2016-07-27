8d31047009172a632c6af90d6bcc382cad886fa5
Bartek Igielski <igloczek@gmail.com>
2016-07-27 16:32:24 +0200

    Improved test performance

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -2,7 +2,7 @@ const gulp   = require('gulp'),
       eslint = require('gulp-eslint');
 
 gulp.task('eslint', () => {
-  return gulp.src(['../**/*.js', '!../**/node_modules/**'])
+  return gulp.src(['*.js', '../*.js', '../task/*.js', '../helper/*.js'])
     .pipe(eslint({ configFile: '.eslintrc' }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
