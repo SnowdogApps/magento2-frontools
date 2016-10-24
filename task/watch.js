c437734768fbc6fb996cd785607ea2fda71a74f9
Bartek Igielski <igloczek@gmail.com>
2016-10-24 10:45:30 +0200

    #77 - Watching more type of files

--- task/watch.js
+++ task/watch.js
@@ -9,6 +9,8 @@ module.exports = function() { // eslint-disable-line func-names
   // Watch files for changes and run appropriate compiler when they change
   themes.forEach(name => {
     const theme = config.themes[name];
+
+    // Style files watching
     theme.locale.forEach(locale => {
       const themePath = theme.default ? theme.dest + '/' + locale : theme.src,
             files = plugins.globby.sync([
@@ -35,8 +37,24 @@ module.exports = function() { // eslint-disable-line func-names
             compiler();
           }
         );
-        dependencyTreeBuilder(theme, file, plugins);
       });
     });
+
+    // Watching files that require reload after save
+    gulp.watch(
+      // I'm usng globby manually, b/c it's a loooot faster
+      plugins.globby.sync([
+        config.projectPath + theme.src + '/**/*.{html,phtml,xml,csv,js}',
+        '!' + config.projectPath + theme.src + '/**/node_modules/**'
+      ]),
+      event => {
+        plugins.util.log(
+          plugins.util.colors.green('File') + ' '
+          + plugins.util.colors.blue(event.path.replace(config.projectPath, '')) + ' '
+          + plugins.util.colors.green('changed.')
+        );
+        plugins.browserSync.reload();
+      }
+    );
   });
 };
