444a0db862a1c3234ea938a6a34eb8c243af6b06
Kirsty Burgoine <kirstyb@fisheyehq.com>
2017-02-09 13:25:35 +0000

    Added sassDoc features

---
+++ task/sassdoc.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = function() { // eslint-disable-line func-names
+
+	const gulp = this.gulp,
+		  config     = this.opts.configs,
+		  plugins    = this.opts.plugins,
+		  themes     = plugins.getThemes(),
+		  sassdoc 	 = require('sassdoc');
+
+	themes.forEach(name => {
+
+	    const theme = config.themes[name],
+			  options = {
+			  	// Path to new folder in theme called docs
+			    dest: config.projectPath + theme.src + '/docs',
+			    verbose: true,
+			    // Optional description at top of every page. 
+			    descriptionPath: config.projectPath + theme.src + '/README.md'
+			  };
+
+	    theme.locale.forEach(locale => {
+	      
+	     	gulp.src(config.projectPath + theme.src + '/styles/**/*.scss')
+	      	.pipe(sassdoc(options));
+	    });
+
+	});
+
+};
