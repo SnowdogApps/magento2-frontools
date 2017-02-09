ee4c8627ecf29710a33856bc69839e5b6b262257
Kirsty Burgoine <kirstyb@fisheyehq.com>
2017-02-09 15:05:09 +0000

    spacing

--- task/sassdoc.js
+++ task/sassdoc.js
@@ -2,25 +2,22 @@
 module.exports = function() { // eslint-disable-line func-names
 
   const gulp = this.gulp,
-	  config     = this.opts.configs,
-    plugins    = this.opts.plugins,
-    themes     = plugins.getThemes(),
-    sassdoc 	 = require('sassdoc');
+        config = this.opts.configs,
+        plugins = this.opts.plugins,
+        themes = plugins.getThemes(),
+        sassdoc = require('sassdoc');
 
   themes.forEach(name => {
 
     const theme = config.themes[name],
-    options = {
-      // Path to new folder in theme called docs
+          options = {
       dest: config.projectPath + theme.src + '/docs',
       verbose: true,
-      // Optional description at top of every page. 
       descriptionPath: config.projectPath + theme.src + '/README.md'
     };
 
-    theme.locale.forEach(locale => {
-      gulp.src(config.projectPath + theme.src + '/styles/**/*.scss')
-      .pipe(sassdoc(options));
-    });
+    gulp.src(config.projectPath + theme.src + '/styles/**/*.scss')
+    .pipe(sassdoc(options));
+
   });
 };
