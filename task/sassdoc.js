3edfc803e3fbb33d7fb6811cbda9dce93850b557
Kirsty Burgoine <kirstyb@fisheyehq.com>
2017-02-09 14:48:33 +0000

    manually deleted tabs

--- task/sassdoc.js
+++ task/sassdoc.js
@@ -1,28 +1,26 @@
 'use strict';
 module.exports = function() { // eslint-disable-line func-names
 
-	const gulp = this.gulp,
-		  config     = this.opts.configs,
-		  plugins    = this.opts.plugins,
-		  themes     = plugins.getThemes(),
-		  sassdoc 	 = require('sassdoc');
+  const gulp = this.gulp,
+	  config     = this.opts.configs,
+    plugins    = this.opts.plugins,
+    themes     = plugins.getThemes(),
+    sassdoc 	 = require('sassdoc');
 
-	themes.forEach(name => {
+  themes.forEach(name => {
 
-	    const theme = config.themes[name],
-			  options = {
-			  	// Path to new folder in theme called docs
-			    dest: config.projectPath + theme.src + '/docs',
-			    verbose: true,
-			    // Optional description at top of every page. 
-			    descriptionPath: config.projectPath + theme.src + '/README.md'
-			  };
+    const theme = config.themes[name],
+    options = {
+      // Path to new folder in theme called docs
+      dest: config.projectPath + theme.src + '/docs',
+      verbose: true,
+      // Optional description at top of every page. 
+      descriptionPath: config.projectPath + theme.src + '/README.md'
+    };
 
-	    theme.locale.forEach(locale => {
-	     	gulp.src(config.projectPath + theme.src + '/styles/**/*.scss')
-	      	.pipe(sassdoc(options));
-	    });
-
-	});
-
+    theme.locale.forEach(locale => {
+      gulp.src(config.projectPath + theme.src + '/styles/**/*.scss')
+      .pipe(sassdoc(options));
+    });
+  });
 };
