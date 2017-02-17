357bd138e17a4d915d8f57a68c03f094a3ec429c
Bartek Igielski <igloczek@gmail.com>
2017-02-17 21:48:52 +0100

    Module styles inheritance and locale overwrites

--- helper/inheritance-resolver.js
+++ helper/inheritance-resolver.js
@@ -52,22 +52,24 @@ module.exports = function(plugins, config, name) { // eslint-disable-line func-n
     // Clean destination dir before generating new symlinks
     plugins.fs.removeSync(themeDest);
 
-    // Create symlinks for theme modules
-    if (theme.modules) {
-      Object.keys(theme.modules).forEach(name => {
-        const moduleSrc = config.projectPath + theme.modules[name];
-        generateSymlinks(
-          moduleSrc,
-          themeDest,
-          [
-            [moduleSrc, '/' + name]
-          ]
-        );
-      });
-    }
 
     // Create symlinks for themes without any per locale modifcations (default)
     if (!theme.localeOverwrites) {
+
+      // Create symlinks for theme modules
+      if (theme.modules) {
+        Object.keys(theme.modules).forEach(name => {
+          const moduleSrc = config.projectPath + theme.modules[name];
+          generateSymlinks(
+            moduleSrc,
+            themeDest,
+            [
+              [moduleSrc, '/' + name]
+            ]
+          );
+        });
+      }
+
       if (theme.parent) {
         const parentSrc = config.projectPath
           + 'var/view_preprocessed/frontools'
@@ -83,6 +85,21 @@ module.exports = function(plugins, config, name) { // eslint-disable-line func-n
     else {
       // We have to handle every locale independly, b/c of possible overwrites
       theme.locale.forEach(locale => {
+        // Create symlinks for theme modules
+        if (theme.modules) {
+          Object.keys(theme.modules).forEach(name => {
+            const moduleSrc = config.projectPath + theme.modules[name];
+            generateSymlinks(
+              moduleSrc,
+              themeDest + '/' + locale,
+              [
+                [moduleSrc, '/' + name]
+              ],
+              ['!/**/i18n/**']
+            );
+          });
+        }
+
         // If theme have parent, create symlinks to all avaliabe files and then overwitte only neccessary
         if (theme.parent) {
           const parentSrc = config.projectPath
@@ -104,6 +121,21 @@ module.exports = function(plugins, config, name) { // eslint-disable-line func-n
           themeSrc,
           ['!/**/i18n/**']
         );
+
+        // Overwritte parent/current modules symlinks with locale specific files
+        if (theme.modules) {
+          Object.keys(theme.modules).forEach(name => {
+            const moduleSrc = config.projectPath + theme.modules[name];
+            generateSymlinks(
+              moduleSrc + '/**/i18n/' + locale,
+              themeDest + '/' + locale,
+              [
+                [moduleSrc, '/' + name],
+                ['/i18n/' + locale, '']
+              ]
+            );
+          });
+        }
 
         // Overwritte parent/current theme symlinks with locale specific files
         generateSymlinks(
