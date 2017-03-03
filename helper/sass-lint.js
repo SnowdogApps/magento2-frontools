c7168fa9aad86bfdae1da3d8f1e3384ba3b5ada9
Bartek Igielski <igloczek@gmail.com>
2017-03-03 18:13:47 +0100

    Fixed huge bug related to handling gulp streams

--- helper/sass-lint.js
+++ helper/sass-lint.js
@@ -1,25 +1,22 @@
 'use strict';
 module.exports = function(gulp, plugins, config, name, file) { // eslint-disable-line func-names
-  // Return function that is executed inside of .pipe()
-  return () => {
-    const theme          = config.themes[name],
-          srcBase        = config.projectPath + 'var/view_preprocessed/frontools' + theme.dest.replace('pub/static', ''),
-          sassLintConfig = require('../helper/config-loader')('sass-lint.yml', plugins, config);
+  const theme          = config.themes[name],
+        srcBase        = config.projectPath + 'var/view_preprocessed/frontools' + theme.dest.replace('pub/static', ''),
+        sassLintConfig = require('../helper/config-loader')('sass-lint.yml', plugins, config);
 
-    return gulp.src(file || plugins.globby.sync(srcBase + '/**/*.scss'))
-      .pipe(plugins.if(
-        !plugins.util.env.ci,
-        plugins.plumber({
-          errorHandler: plugins.notify.onError('Error: <%= error.message %>')
-        })
-      ))
-      .pipe(plugins.sassLint(sassLintConfig))
-      .pipe(plugins.sassLint.format())
-      .pipe(plugins.if(plugins.util.env.ci, plugins.sassLint.failOnError()))
-      .pipe(plugins.if(file, plugins.logger({
-        display   : 'name',
-        beforeEach: 'Theme: ' + name + ' ' + 'File: ',
-        afterEach : ' - SASS Lint finished.'
-      })));
-  }
+  return gulp.src(file || plugins.globby.sync(srcBase + '/**/*.scss'))
+    .pipe(plugins.if(
+      !plugins.util.env.ci,
+      plugins.plumber({
+        errorHandler: plugins.notify.onError('Error: <%= error.message %>')
+      })
+    ))
+    .pipe(plugins.sassLint(sassLintConfig))
+    .pipe(plugins.sassLint.format())
+    .pipe(plugins.if(plugins.util.env.ci, plugins.sassLint.failOnError()))
+    .pipe(plugins.if(file, plugins.logger({
+      display   : 'name',
+      beforeEach: 'Theme: ' + name + ' ' + 'File: ',
+      afterEach : ' - SASS Lint finished.'
+    })));
 };
