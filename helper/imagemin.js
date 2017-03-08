061f484da0e62ab704b0bb29c9c8cb3945b0ba7c
claireparker <claire.parker87@gmail.com>
2017-03-08 14:46:17 +0000

    Put imageMin config into external file
    Adds a try-catch so that if the user doesn't have a config file set up
    then they will get some default config options.

--- helper/imagemin.js
+++ helper/imagemin.js
@@ -4,14 +4,26 @@ module.exports = function(gulp, plugins, config, name) { // eslint-disable-line 
   return () => {
     const theme          = config.themes[name],
           srcBase        = config.projectPath + theme.src,
-          imageDir = srcBase + '/web/images/*',
-          minnedDir = srcBase + '/web/images/',
-        //   gulpiconConfig = require('../helper/config-loader')('gulpicon.json', plugins, config);
-          imageMinConfig = { optimizationLevel: 3, progressive: true, interlaced: true };
 
+          // Look for any images in the web/images directory and minify them in place
+          imageDir       = srcBase + '/web/images/**/*',
+          minnedImageDir = srcBase + '/web/images/';
+
+    let imageMinConfig;
+
+    try {
+        // Checks for project specific config in dev/tools/frontools/config
+        imageMinConfig = require('../helper/config-loader')('imageMinConfig.json', plugins, config);
+    } catch (e) {
+        imageMinConfig = { "optimizationLevel": 3, "progressive": true, "interlaced": true };
+    }
 
     return gulp.src(imageDir)
         .pipe(plugins.imagemin(imageMinConfig))
-        .pipe(gulp.dest(minnedDir));
+        .pipe(gulp.dest(minnedImageDir));
   }
 };
+Put imageMin config into external file
+
+Adds a try-catch so that if the user doesn't have a config file set up
+then they will get some default config options.
