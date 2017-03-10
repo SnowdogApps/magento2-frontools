067d5bf034863088ebb79a32595eb6b4955dbeeb
claireparker <claire.parker87@gmail.com>
2017-03-10 09:05:58 +0000

    Remove try-catch and add sample config data
    Don't need try-catch to check for config file and provide a fallback -
    this is not how config-helper is designed to work. Instead we should
    prvide a sample file in the config directory which will be copied to the
    local project on `gulp setup`. Then if this doesn't exist, the gulp task
    should correctly fail.

--- helper/imagemin.js
+++ helper/imagemin.js
@@ -4,19 +4,12 @@ module.exports = function(gulp, plugins, config, name) { // eslint-disable-line 
   return () => {
     const theme          = config.themes[name],
           srcBase        = config.projectPath + theme.src,
+          imageMinConfig = require('../helper/config-loader')('imagemin.json', plugins, config),
 
           // Look for any images in the web/images directory and minify them in place
           imageDir       = srcBase + '/web/images/**/*',
           minnedImageDir = srcBase + '/web/images/';
 
-    let imageMinConfig;
-
-    try {
-        // Checks for project specific config in dev/tools/frontools/config
-        imageMinConfig = require('../helper/config-loader')('imageMinConfig.json', plugins, config);
-    } catch (e) {
-        imageMinConfig = { "optimizationLevel": 3, "progressive": true, "interlaced": true };
-    }
 
     return gulp.src(imageDir)
         .pipe(plugins.imagemin(imageMinConfig))
