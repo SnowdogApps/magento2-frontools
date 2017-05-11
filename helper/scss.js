69c76d4aac482bd65f0a6d101021ee441584e53f
Gil Greenberg <ggreenberg@somethingdigital.com>
2017-05-11 16:35:27 -0400

    Do not add min suffix in production mode on a per theme basis

--- helper/scss.js
+++ helper/scss.js
@@ -1,11 +1,12 @@
 'use strict';
 module.exports = function(gulp, plugins, config, name, file) { // eslint-disable-line func-names
-  const theme       = config.themes[name],
-        srcBase     = config.projectPath + 'var/view_preprocessed/frontools' + theme.dest.replace('pub/static', ''),
-        stylesDir   = theme.stylesDir ? theme.stylesDir : 'styles',
-        disableMaps = plugins.util.env.disableMaps || false,
-        production  = plugins.util.env.prod || false,
-        postcss     = [];
+  const theme         = config.themes[name],
+        srcBase       = config.projectPath + 'var/view_preprocessed/frontools' + theme.dest.replace('pub/static', ''),
+        stylesDir     = theme.stylesDir ? theme.stylesDir : 'styles',
+        disableMaps   = plugins.util.env.disableMaps || false,
+        production    = plugins.util.env.prod || false,
+        postcss       = [],
+        disableSuffix = theme.disableSuffix || false;
 
   if (theme.postcss) {
     theme.postcss.forEach(el => {
@@ -52,7 +53,7 @@ module.exports = function(gulp, plugins, config, name, file) { // eslint-disable
       .pipe(plugins.if(production, plugins.postcss([plugins.cssnano()])))
       .pipe(plugins.if(postcss.length, plugins.postcss(postcss || [])))
       .pipe(plugins.if(!disableMaps && !production, plugins.sourcemaps.write()))
-      .pipe(plugins.if(production, plugins.rename({ suffix: '.min' })))
+      .pipe(plugins.if(production && !disableSuffix, plugins.rename({ suffix: '.min' })))
       .pipe(plugins.rename(adjustDestinationDirectory))
       .pipe(plugins.multiDest(dest))
       .pipe(plugins.logger({
@@ -86,7 +87,7 @@ module.exports = function(gulp, plugins, config, name, file) { // eslint-disable
           .pipe(plugins.if(production, plugins.postcss([plugins.cssnano()])))
           .pipe(plugins.if(postcss.length, plugins.postcss(postcss || [])))
           .pipe(plugins.if(!disableMaps && !production, plugins.sourcemaps.write()))
-          .pipe(plugins.if(production, plugins.rename({ suffix: '.min' })))
+          .pipe(plugins.if(production && !disableSuffix, plugins.rename({ suffix: '.min' })))
           .pipe(plugins.rename(adjustDestinationDirectory))
           .pipe(gulp.dest(config.projectPath + theme.dest + '/' + locale))
           .pipe(plugins.logger({
