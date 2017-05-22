78344c0fb384a4600b6daf2a474ca203ad7aa776
Rhodri George <rhodri.george@ampersandit.co.uk>
2017-05-22 10:44:20 +0100

    add initial version of webpack building

---
+++ task/webpack-build.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function() { // eslint-disable-line func-names
+  // Global variables
+  const gulp    = this.gulp,
+        plugins = this.opts.plugins,
+        config  = this.opts.configs,
+        themes  = plugins.getThemes(),
+        streams = plugins.mergeStream(),
+        webpack = this.webpack;
+
+  // Loop through themes to compile scss or less depending on your config.json
+  themes.forEach(name => {
+    streams.add(require('../helper/webpack-build')(gulp, plugins, config, name));
+  });
+
+  return streams;
+};
---
+++ task/webpack.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function() { // eslint-disable-line func-names
+  // Global variables
+  const gulp    = this.gulp,
+        plugins = this.opts.plugins,
+        config  = this.opts.configs,
+        themes  = plugins.getThemes(),
+        streams = plugins.mergeStream();
+
+  plugins.runSequence('inheritance', 'webpack', 'webpackdist');
+
+  return streams;
+};
