0fb612761550782cac2e13ecaed0aa9cf5aa88ed
Vincent <vincent@headline-interactive.nl>
2017-06-28 09:10:44 +0200

    Fixed extension, both scss and sass

--- task/watch.js
+++ task/watch.js
@@ -53,8 +53,8 @@ module.exports = function(resolve) { // eslint-disable-line func-names
 
       // Find all main SASS files
       plugins.globby.sync([
-        themeTempSrc + '/**/*.sass',
-        '!/**/_*.sass'
+        themeTempSrc + '/**/*\.(sass|scss)',
+        '!/**/_*\.(sass|scss)'
       ]).forEach(file => {
         // Generate array of main file dependecies
         sassDependecyTree[file] = plugins.helper.dependecyTree(plugins, file);
@@ -133,13 +133,13 @@ module.exports = function(resolve) { // eslint-disable-line func-names
 
       // SASS Lint
       if (!plugins.util.env.disableLinting) {
-        if (plugins.path.extname(path) === '.sass') {
+        if (plugins.path.extname(path) === '.sass' || plugins.path.extname(path) === '.scss') {
           plugins.helper.sassLint(gulp, plugins, config, name, path);
         }
       }
 
       // SASS Compilation
-      if (plugins.path.extname(path) === '.sass') {
+      if (plugins.path.extname(path) === '.sass' || plugins.path.extname(path) === '.scss') {
         Object.keys(sassDependecyTree).forEach(file => {
           if (sassDependecyTree[file].includes(path)) {
             plugins.helper.sass(gulp, plugins, config, name, file);
