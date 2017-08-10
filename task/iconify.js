48db13f09a39937d9f953b2fa252279b8f470627
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2017-08-10 09:42:46 +0100

    Add iconify task

---
+++ task/iconify.js
@@ -0,0 +1,63 @@
+'use strict';
+module.exports = function() { // eslint-disable-line func-names
+  const gulp = this.gulp,
+        plugins = this.opts.plugins,
+        config  = this.opts.configs,
+        themes  = plugins.getThemes();
+
+    gulp.task('iconClean', function (callback) {
+        /*  Synchronously remove all assets before recompile, as `iconify` task
+            doesn't support force: true on del */
+        var allThemes = [];
+
+        themes.forEach(name => {
+            const theme = config.themes[name];
+
+            allThemes.push('../' + theme.src + '/web/icons/png/*');
+            allThemes.push('../' + theme.src + '/web/icons/scss/*');
+            allThemes.push('../' + theme.src + '/web/icons/css/*');
+        });
+
+        plugins.del.sync(allThemes, {
+            force: true
+        });
+
+        callback();
+    });
+
+    gulp.task('iconBuild', function () {
+        themes.forEach(name => {
+            const theme = config.themes[name];
+
+            plugins.iconify(    {
+                src: '../' + theme.src + '/web/icons/svg/*.svg',
+                pngOutput: '../' + theme.src + '/web/icons/png',
+                scssOutput: '../' + theme.src + '/web/icons/scss',
+                cssOutput:  '../' + theme.src + '/web/icons/css',
+                defaultWidth: '40px',
+                defaultHeight: '40px',
+                svgoOptions: {
+                    enabled: true,
+                    options: {
+                        plugins: [
+                            { removeUnknownsAndDefaults: false },
+                            { mergePaths: false }
+                        ]
+                    }
+                },
+                svg2pngOptions: {
+                    options: {
+                        width: '40px',
+                        height: '40px',
+                    },
+                    scaling: 1.0,
+                    concurrency: null
+                }
+            });
+        });
+    });
+
+    gulp.task('iconify', function() {
+        gulp.start('iconClean', 'iconBuild');
+    });
+};
