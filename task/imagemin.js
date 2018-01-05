4193145c1dee020c93511b19074eaa618c2fb231
claireparker <claireparker@users.noreply.github.com>
2018-01-05 10:19:33 +0000

    Add --dir flag to imagemin task

--- task/imagemin.js
+++ task/imagemin.js
@@ -6,12 +6,20 @@ module.exports = function() { // eslint-disable-line func-names
         config  = this.opts.configs,
         themes  = plugins.getThemes();
 
-  themes.forEach(name => {
-    plugins.util.log(
-      plugins.util.colors.green('Running imagemin on') + ' '
-      + plugins.util.colors.blue(name) + ' '
-      + plugins.util.colors.green('theme...')
-    );
-    require('../helper/imagemin')(gulp, plugins, config, name)();
-  });
+
+  if (plugins.util.env.dir) {
+      plugins.util.log(
+        plugins.util.colors.green('Running imagemin on custom directory...')
+      );
+      require('../helper/imagemin')(gulp, plugins, config)();
+  } else {
+      themes.forEach(name => {
+        plugins.util.log(
+          plugins.util.colors.green('Running imagemin on') + ' '
+          + plugins.util.colors.blue(name) + ' '
+          + plugins.util.colors.green('theme...')
+        );
+        require('../helper/imagemin')(gulp, plugins, config, name)();
+      });
+  }
 };
