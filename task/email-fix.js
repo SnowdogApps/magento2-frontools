148d589576e4248e8af194e0961227eb62fd8e01
Bartek Igielski <igloczek@gmail.com>
2018-12-18 12:59:15 +0100

    Update email-fix.js

--- task/email-fix.js
+++ task/email-fix.js
@@ -1,21 +1,21 @@
-'use strict';
-module.exports = function() { // eslint-disable-line func-names
+'use strict'
+module.exports = () => {
   // Global variables
-  const gulp     = this.gulp,
-        plugins  = this.opts.plugins,
-        config   = this.opts.configs,
-        themes   = plugins.getThemes(),
-        streams  = plugins.mergeStream(),
-        emailFix = require('../helper/email-fix');
+  const gulp     = this.gulp
+  const plugins  = this.opts.plugins
+  const config   = this.opts.configs
+  const themes   = plugins.getThemes()
+  const streams  = plugins.mergeStream()
+  const emailFix = require('../helper/email-fix')
 
   themes.forEach(name => {
     plugins.util.log(
       plugins.util.colors.green('Runing email-fix on') + ' '
       + plugins.util.colors.blue(name) + ' '
       + plugins.util.colors.green('theme...')
-    );
-    streams.add(emailFix(gulp, plugins, config, name));
-  });
+    )
+    streams.add(emailFix(gulp, plugins, config, name))
+  })
 
-  return streams;
-};
+  return streams
+}
