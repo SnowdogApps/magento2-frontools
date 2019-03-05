f834a6d7165b5ea0370677581ae12c1488f7bde1
Aurélien Lavorel <aurel.lavorel@gmail.com>
2019-03-05 21:36:57 +0100

    fix review

--- task/prod.js
+++ task/prod.js
@@ -2,7 +2,6 @@
 module.exports = function() { // eslint-disable-line func-names
   // Global variables
   const plugins = this.opts.plugins,
-        config  = this.opts.configs,
         env     = plugins.util.env;
   env.prod = true;
   env.disableMaps = true;
