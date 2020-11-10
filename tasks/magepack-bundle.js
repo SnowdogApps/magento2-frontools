692d41dc3992144b14b8cf925d6f0fd06596d346
Bartek Igielski <igloczek@gmail.com>
2020-11-10 19:25:27 +0100

    Bunlde just themes listed in config with option to bunlde just one

--- tasks/magepack-bundle.js
+++ tasks/magepack-bundle.js
@@ -1,8 +1,20 @@
 import bundle from 'magepack/lib/bundle'
 
-import { env } from '../helpers/config'
+import getThemes from '../helpers/get-themes'
+import { env, projectPath, themes } from '../helpers/config'
+
+function getThemesGlobPattern() {
+  const themesNames = getThemes()
+
+  if (themesNames.length === 1) {
+    return `${projectPath}${themes[themesNames[0]].dest}/*`
+  }
+
+  return `${projectPath}{${themesNames.map(name => themes[name].dest).join(',')}}/*`
+}
 
 export default async function() {
   const configPath = env.c || env.config
-  await bundle(configPath)
+  const themesGlobPattern = getThemesGlobPattern()
+  await bundle(configPath, themesGlobPattern)
 }
